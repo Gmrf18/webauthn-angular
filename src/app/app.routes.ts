9199fff7bdb90ac31e756b512ca0963a020f55c2
gmrf18 <gmrf18@gmail.com>
2025-08-26 07:01:32 -0600

    add routes 🚧

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'signin',
+    loadComponent: () => import('./signin/signin'),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login'),
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
